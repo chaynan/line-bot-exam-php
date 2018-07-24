5be1b844605a8628508f1609c6a602bca8c35e0c
LAPTOP-BK8GEDTR\user <chaynan70@hotmail.com>
2018-07-24 09:00:47 +0700

    1

--- webhooks.php
+++ webhooks.php
@@ -3,8 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
-
+$access_token = 'p3q2WU/8vwZu9yvZgXNck1tETJt7yKJJBlO3ids+DtPqLCONHPBliUpNg01tnQFvrAj/KnSPkT2gF9BoLtbPEV37TOHBjxEc1Qfssgnyfn8GfhvvVtcTWQoNvVMPqFEGCEovufnxx9kj2Xq3ZBaJVQdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
