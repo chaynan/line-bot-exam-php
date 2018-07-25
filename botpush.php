a9076b036e35a26cd7b0a81c799fcbcd4f975dce
LAPTOP-586KOAKM\chayn <chyanan70@hotmail.com>
2018-07-25 13:19:35 +0700

    แก้3

--- botpush.php
+++ botpush.php
@@ -10,10 +10,13 @@ $channelSecret ='0e41498c3832c3a4c532816535d95396';
 
 $pushID ='Uce5927743da4ab348b5a528dc43546bd';
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$httpClient = new 
+\LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 
+$channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$textMessageBuilder = new 
+\LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
