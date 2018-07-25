1db4fccb60e18a944d703c2d4b5e0c4a0d35e251
LAPTOP-586KOAKM\chayn <chyanan70@hotmail.com>
2018-07-25 11:29:46 +0700

    แก้ 1

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'cjeGRATSP3/jMqU5ohVifDBsGcjAn62aIjXj/pUB7+H35FCGzIvciSnNQG8ZBrcmJDp6/ndxgg1l4w8sirSQfgCPA+lfU2DhSI0nAFCJUlnUvbMbOn2PzqOwwvRrOjSJNaU5DPELAof1xSyr01Tf9QdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
